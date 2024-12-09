ecc6667f101b45ccfa9f48a21ae3498658175eb9
mbuffetti <130080098+mbuffetti@users.noreply.github.com>
2024-12-09 03:16:34 +0000

    F fix error on build

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,14 +4,11 @@ import '@/styles/components/header.css'
 
 import { useState } from 'react'
 
-import Header from '@/components/Navigation/Header'
 import Sidebar from '@/components/Navigation/Sidebar'
 
 export default function Navigation() {
-  const [isOpenSidebar, setIsOpenSidebar] = useState(false)
   return (
     <div data-testid="navigation-bar">
-      <Header handleOpenSidebar={() => setIsOpenSidebar(!isOpenSidebar)} />
       <Sidebar />
     </div>
   )
